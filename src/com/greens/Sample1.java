00c6833a215a156501a297c5397775a45b9537d6
YuvarajSekar3 <yuvi.sekar3@gmail.com>
2019-02-12 22:36:04 +0530

    Day1

--- src/com/greens/Sample1.java
+++ src/com/greens/Sample1.java
@@ -3,11 +3,16 @@ package com.greens;
 public class Sample1 {
 	public void emp() {
 		// TODO Auto-generated method stub
+		System.out.println("test1");
 
 	}
 
 	public void emp1() {
 		// TODO Auto-generated method stub
-
+System.out.println("test2");
+	}
+	
+	public static void main(String[] args) {
+		Sample1  s = new Sample1();
 	}
 }
