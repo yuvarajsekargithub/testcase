15562d41074f558db1f9a13f67ee23ac5f43a3c1
YuvarajSekar3 <yuvi.sekar3@gmail.com>
2019-02-12 21:48:18 +0530

    firstcommit

---
+++ src/com/greens/Sample1.java
@@ -0,0 +1,13 @@
+package com.greens;
+
+public class Sample1 {
+	public void emp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void emp1() {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ src/com/greens/Sample2.java
@@ -0,0 +1,14 @@
+package com.greens;
+
+public class Sample2 {
+	private void test1() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void test2() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
